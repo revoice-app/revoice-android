34e2cc515e2f01803a11caf58cf054d9579ffa48
Abhinav  Gupta <gabhi8740@gmail.com>
2020-12-20 22:53:12 +0530

    ini1

---
+++ app/src/main/java/com/sar/user/smart_city/Splash.java
@@ -0,0 +1,25 @@
+package com.sar.user.smart_city;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,MainScreen.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, 3000);
+    }
+}
