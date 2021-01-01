6f8c6be8ad1fc4fd19731b92df8a4e29392aa0f9
Taru Ghiya <30281145+tarughiya@users.noreply.github.com>
2021-01-01 23:06:46 +0530

    commit

--- app/src/main/java/com/sar/user/smart_city/FinalAct.java
+++ app/src/main/java/com/sar/user/smart_city/FinalAct.java
@@ -4,19 +4,19 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -26,20 +26,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.tensorflow.lite.examples.textclassification.client.Result;
 import org.tensorflow.lite.examples.textclassification.client.TextClassificationClient;
 
-import java.util.List;
 import java.util.Random;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class FinalAct extends AppCompatActivity {
     TextView textView;
     private TextClassificationClient client;
     private MaterialButton materialButton;
-
     private Handler handler;
     private static ProgressDialog progress;
     private ImageView imageView;
@@ -51,33 +45,26 @@ public class FinalAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
-        textView=findViewById(R.id.txvResult);
-        imageView=findViewById(R.id.image);
-        textView2=findViewById(R.id.textClass);
-
+        textView = findViewById(R.id.txvResult);
+        imageView = findViewById(R.id.image);
+        textView2 = findViewById(R.id.textClass);
         client = new TextClassificationClient(getApplicationContext());
         handler = new Handler();
-        Intent intent=getIntent();
-        Random random=new Random();
-        int aa=random.nextInt(100000);
-        firebaseDatabase=FirebaseDatabase.getInstance();
-        databaseReference=firebaseDatabase.getReference("review/"+ FirebaseAuth.getInstance().getUid().toString()+"/"+String.valueOf(aa));
-
-        String text=intent.getStringExtra("text");
-        String textclass=intent.getStringExtra("class");
+        Intent intent = getIntent();
+        Random random = new Random();
+        int aa = random.nextInt(100000);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("review/" + FirebaseAuth.getInstance().getUid().toString() + "/" + String.valueOf(aa));
+        String text = intent.getStringExtra("text");
+        String textclass = intent.getStringExtra("class");
         textView.setText(text);
         textView2.setText(textclass);
-
-
-        byte[] d=intent.getByteArrayExtra("image");
-        if(d.length>0) {
+        byte[] d = intent.getByteArrayExtra("image");
+        if (d.length > 0) {
             imageView.setImageBitmap(BitmapFactory.decodeByteArray(d, 0, d.length));
         }
-
         //classify("nice");
-
-
-        materialButton=findViewById(R.id.button);
+        materialButton = findViewById(R.id.button);
         materialButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,24 +73,21 @@ public class FinalAct extends AppCompatActivity {
                 databaseReference.child("image").setValue(Base64.encodeToString(d, Base64.DEFAULT)).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(FinalAct.this,"Something went wrong",Toast.LENGTH_SHORT).show();
-
-                        startActivity(new Intent(FinalAct.this,MainScreen.class));
-
+                        Toast.makeText(FinalAct.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(FinalAct.this, MainScreen.class));
                     }
                 });
                 databaseReference.child("textClass").setValue(textclass).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(FinalAct.this,"Done! Thank you",Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(FinalAct.this,MainScreen.class));
+                        Toast.makeText(FinalAct.this, "Done! Thank you", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(FinalAct.this, MainScreen.class));
                     }
                 });
             }
         });
-
-
     }
+
     public static void showLoader(Context context) {
         hideLoader(context);
         if (null != context && !(((Activity) context).isFinishing())) {
@@ -114,6 +98,7 @@ public class FinalAct extends AppCompatActivity {
             progress.show();
         }
     }
+
     public static void hideLoader(Context context) {
         if (null != progress && progress.isShowing() && !(((Activity) context).isFinishing())) {
             progress.hide();
@@ -121,7 +106,4 @@ public class FinalAct extends AppCompatActivity {
         }
         progress = null;
     }
-
 }
-
-
